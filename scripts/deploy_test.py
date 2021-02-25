395de688c1f420475c95d18d1eb5930ddb940054
Witchayut Jongpattanasombut <skpepe134@gmail.com>
2021-02-25 16:57:11 +0700

    change pools to be function params

--- scripts/deploy_test.py
+++ scripts/deploy_test.py
@@ -5,29 +5,30 @@ from .utils import *
 import eth_abi
 from .constant import *
 
-def deploy(bank_config, bank, oracle, add_strat, liq_strat, rem_strat, goblin_config):
+def deploy_goblin(admin, pools, bank_config, bank, oracle, add_strat, liq_strat, rem_strat, goblin_config):
     wbnb = interface.IAny(wbnb_address)
 
     res = []
     for pool in pools:
         fToken = interface.IAny(pool["token"])
-        price = (10**18 * wbnb.balanceOf(pool["lp"])) // cake.balanceOf(pool["lp"])
+        price = (10**18 * wbnb.balanceOf(pool["lp"])) // fToken.balanceOf(pool["lp"])
         oracle.setPrices([fToken], [wbnb], [price], {'from': admin})
+
         goblin = PancakeswapGoblin.deploy(
             bank, chef_address, router_address, pool["pid"], add_strat, liq_strat, 300, {'from': admin})
         goblin_config.setConfigs([goblin], [pool["goblinConfig"]], {'from': admin})
+        add_strat_2 = StrategyAddTwoSidesOptimal.deploy(router_address, goblin, {'from': admin})
+        goblin.setStrategyOk([add_strat_2], True, {'from': admin})
         bank_config.setGoblins([goblin], [goblin_config], {'from': admin})
+
         res.append({
             "pool": pool,
             "goblin": goblin,
+            "twoSide": add_strat_2
         })
     return res
 
-
-def main():
-    admin = accounts[0]
-    alice = accounts[1]
-
+def deploy(admin, pools):
     triple_slope_model = TripleSlopeModel.deploy({'from': admin})
 
     # min debt 0.2 BNB at 10 gwei gas price (killBps 5% -> at least 0.01BNB bonus)
@@ -44,25 +45,37 @@ def main():
     liq_strat = StrategyLiquidate.deploy(router_address, {'from': admin})
     rem_strat = StrategyWithdrawMinimizeTrading.deploy(router_address, {'from': admin})
 
-    # new strat
-    add_strat_2 = StrategyAddTwoSidesOptimal.deploy(router_address, goblin, {'from': admin})
-    goblin.setStrategyOk([add_strat_2], True, {'from': admin})
-
-    # set goblin in bank config
-    # work factor 72.5%
-    # kill factor 80%
-    # max price diff 10%
     goblin_config = PancakeswapGoblinConfig.deploy(oracle, {'from': admin})
     
-    res = deploy(bank_config, bank, oracle, add_strat, liq_strat, rem_strat, goblin_config)
+    results = deploy_goblin(admin, pools, bank_config, bank, oracle, add_strat, liq_strat, rem_strat, goblin_config)
 
-    for i in res:
-        print(i['pool']['name'], i['goblin'].address)
+    print('bank', bank.address)
+    print('bank_config', bank_config.address)
+    print('all bnb', add_strat.address)
+    print('liq', liq_strat.address)
+    print('withdraw', rem_strat.address)
+
+    for res in results:
+        print(res['pool']['name'], res['goblin'].address, res['twoSide'].address)
+
+    return bank, add_strat, liq_strat, rem_strat, results
+
+
+def main():
+    admin = accounts[0]
+    alice = accounts[1]
+
+    deployed_pools = list(filter(lambda pool: pool['name'] == 'cake', pools))
+    bank, add_strat, liq_strat, rem_strat, results = deploy(admin, deployed_pools)
 
     bank.deposit({
         "from": admin,
         "value": '100 ether'
     })
+
+    wbnb = interface.IAny(wbnb_address)
+    cake = interface.IAny(cake_address)
+    cake_goblin = next((res["goblin"] for res in results if res["pool"]["token"] == cake_address), None)
 
     # mint tokens
     mint_tokens(cake, alice)
@@ -74,15 +87,14 @@ def main():
 
     ###########################################################
     # work
+    prevBNBBal = alice.balance()
 
-    # prevBNBBal = alice.balance()
+    bank.work(0, cake_goblin, 0, 0, eth_abi.encode_abi(['address', 'bytes'], [
+              add_strat.address, eth_abi.encode_abi(['address', 'uint256'], [cake.address, 0])]), {'from': alice, 'value': '1 ether'})
 
-    # bank.work(0, goblin, 0, 0, eth_abi.encode_abi(['address', 'bytes'], [
-    #           add_strat.address, eth_abi.encode_abi(['address', 'uint256'], [cake.address, 0])]), {'from': alice, 'value': '1 ether'})
+    curBNBBal = alice.balance()
 
-    # curBNBBal = alice.balance()
+    print('∆ bnb alice', curBNBBal - prevBNBBal)
+    print('alice pos', bank.positionInfo(1))
 
-    # print('∆ bnb alice', curBNBBal - prevBNBBal)
-    # print('alice pos', bank.positionInfo(1))
-
-    # pass
+    pass
