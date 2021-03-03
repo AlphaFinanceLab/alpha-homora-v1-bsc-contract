65b13444939e3f3f99a9db0818b08992480478b1
n1punp <disney31215@gmail.com>
2021-03-03 07:39:22 +0000

    Update utils script

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,6 +7,7 @@ except:
 CAKE = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82'
 WBNB = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
 ALPHA = '0xa1faa113cbE53436Df28FF0aEe54275c13B40975'
+BUSD = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
 
 
 def almostEqual(a, b, thresh=0.01):
@@ -28,7 +29,7 @@ def mint_tokens(token, to, interface=None, amount=None):
     elif token == WBNB:
         token.deposit({'from': accounts[9], 'value': amount})
         token.transfer(to, amount, {'from': accounts[9]})
-    elif token == ALPHA:
+    elif token == ALPHA or token == BUSD:
         owner = token.owner()
         token.mint(amount, {'from': owner})
         token.transfer(to, amount, {'from': owner})
